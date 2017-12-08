57db775ddceee895402f1d3420ae631ab12469af
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-08 17:20:00 -0500

    adding in meeting and logic

--- app/services/response_service.rb
+++ app/services/response_service.rb
@@ -1,9 +1,10 @@
 class ResponseService
 	def initialize(user:, message:)
+		@user = user
 	end
 
 	def new_meeting(name:)
-		Meeting.new(user_id: user.id)
+		Meeting.new(user_id: @user.id)
 		return ["Date/time & place type?"]
 	end
 end
