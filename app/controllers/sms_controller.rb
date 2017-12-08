e87b6d5cb31e2b9be055643deea03e70a0950fc6
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-08 16:10:55 -0500

    updates for twilio

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,19 +4,11 @@ class SmsController < ApplicationController
 	def reply
 		message_body = params["Body"]
 		from_number = params["From"]
-		boot_twilio
-		sms = @client.messages.create(
-			from: Rails.application.secrets.twilio_number,
+		client = Twilio::REST::Client.new 'AC159ea454d65ec3c5d443d7da8d4b5644', '301b4fd074ff17b541a42962725bab3c'
+		sms = client.messages.create(
+			from: '+16467599030',
 			to: from_number,
 			body: "Hello there, thanks for texting me. Your number is #{from_number}."
 		)
-	end
- 
-  	private
- 
-	def boot_twilio
-		account_sid = Rails.application.secrets.twilio_sid
-		auth_token = Rails.application.secrets.twilio_token
-		@client = Twilio::REST::Client.new account_sid, auth_token
 	end
 end
