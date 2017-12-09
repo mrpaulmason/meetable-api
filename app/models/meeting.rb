a49b391335a3e38ea33059f758d02a831118de2b
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-09 13:42:12 -0500

    adding in default share code value to Meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,2 +1,8 @@
 class Meeting < ApplicationRecord
+	validates :share_code, uniqueness: true
+	before_create :generate_share_code
+
+	def generate_share_code
+		self.share_code = SecureRandom.hex(3)
+	end
 end
