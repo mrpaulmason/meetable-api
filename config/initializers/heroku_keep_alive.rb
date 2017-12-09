f9dcfa9a16d847164de3d2d32dd7e46e711d10cc
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-09 12:39:44 -0500

    updating ping url for rufus

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -5,6 +5,6 @@ if Rails.env.production?
   scheduler.every '10m' do
      require "net/http"
      require "uri"
-     Net::HTTP.get_response(URI.parse(ENV["HOSTNAME"]))
+     Net::HTTP.get_response(URI.parse(ENV["PING"]))
   end
 end
