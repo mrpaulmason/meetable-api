b2d181fed5bbe24e4650526d5217176a04f883a2
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-09 16:08:04 -0500

    fixing share url

--- app/services/response_service.rb
+++ app/services/response_service.rb
@@ -8,6 +8,6 @@ class ResponseService
 		arr = @message.split(",")
 		m = Meeting.new(user_id: @user.id, time: arr[2], date: arr[3], location_type: arr[4])
 		m.save
-		["Send this link to #{arr[1].strip.capitalize}:","http://meetable.com/#{m.share_code}"]
+		["Send this link to #{arr[1].strip.capitalize}:","http://meetable.ai/#{m.share_code}"]
 	end
 end
