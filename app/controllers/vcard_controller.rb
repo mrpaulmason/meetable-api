5ad738a21b90896fe01b3c7556a192594284ab11
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-15 17:30:50 -0500

    updating link

--- app/controllers/vcard_controller.rb
+++ app/controllers/vcard_controller.rb
@@ -2,7 +2,7 @@ class VcardController < ApplicationController
 	def card
 		vcard = VCardigan.create
 		vcard.name 'Meetable'
-		vcard.phone '6467599030'
+		vcard.telephone '6467599030'
 		vcard.fullname 'Meetable.ai'
 		vcard.photo 'https://media.licdn.com/mpr/mpr/shrinknp_100_100/AAEAAQAAAAAAAAbnAAAAJDFmOTJkNjc2LTZhZWYtNDAxMS04Nzc4LWU1YTFmNjZiMzhiNQ.jpg', :type => 'uri'
 		vcard[:item1].url 'https://meetable.ai'
