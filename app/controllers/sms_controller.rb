da5367a6f582f47d02c9e88e4e91485bf608db83
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-18 17:58:50 -0500

    fixing error messages and vcard

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -25,22 +25,32 @@ class SmsController < ApplicationController
 	end
 
 	def accept 
-		no_ref_msg = {:status => 400, :message => 'Could not find referal'}
-		render :json => no_ref_msg unless Meeting.find_by_share_code(params['ref']) 
+		no_ref_msg = {:status => 400, :message => 'Sorry, this code is invalid.'}
+		unless Meeting.find_by_share_code(params['ref']) 
+			render :json => no_ref_msg 
+			return 
+		end
 		
-		no_user_msg = {:status => 400, :message => 'Could not create user'}
-		render :json => no_user_msg unless params.has_key?("phone_number") 
+		no_user_msg = {:status => 400, :message => 'Sorry, please provide a valid phone number.'}
+		unless params.has_key?("phone_number") 
+			render :json => no_user_msg
+			return
+		end
 		
 		user = User.find_or_create_by(phone_number: params["phone_number"])
 		meeting = Meeting.find_by_share_code(params['ref'])
 		meeting.invitee_id = user.id
-		meeting.save
+		if meeting.save
+			client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
+			client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, body: "Welcome to Meetable!")
+			client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, media_url: "https://nameless-bastion-55310.herokuapp.com/vcard")
+			
+			msg = {:status => 200, :message => "OK"}
+			render :json => msg
+			return
+		end
 
-		client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-		client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, body: "Welcome to Meetable!")
-		client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, media_url: "https://nameless-bastion-55310.herokuapp.com/vcard")
-
-		msg = {:status => 200, :message => ""}
-		render :json => msg
+		error_msg = {:status => 400, :message => 'There was an error, please try again'}
+		render error_msg
 	end
 end
