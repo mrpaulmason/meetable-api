f27da1bfd84a8122d1774dac3b431ed62d6c2746
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-18 20:30:08 -0500

    cleaning up error handling

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -26,31 +26,28 @@ class SmsController < ApplicationController
 
 	def accept 
 		no_ref_msg = {:status => 400, :message => 'Sorry, this code is invalid.'}
-		unless Meeting.find_by_share_code(params['ref']) 
+		no_user_msg = {:status => 400, :message => 'Sorry, please provide a valid phone number.'}
+		error_msg = {:status => 400, :message => 'There was an error, please try again'}
+		ok_msg = {:status => 200, :message => "OK"}
+
+		unless meeting = Meeting.find_by_share_code(params['ref']) 
 			render :json => no_ref_msg 
 			return 
 		end
 		
-		no_user_msg = {:status => 400, :message => 'Sorry, please provide a valid phone number.'}
-		unless params.has_key?("phone_number") 
+		unless user = User.find_or_create_by(phone_number: params["phone_number"])
 			render :json => no_user_msg
 			return
 		end
 		
-		user = User.find_or_create_by(phone_number: params["phone_number"])
-		meeting = Meeting.find_by_share_code(params['ref'])
 		meeting.invitee_id = user.id
 		if meeting.save
 			client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
 			client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, body: "Welcome to Meetable!")
 			client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, media_url: "https://meetable-api.herokuapp.com/vcard")
-			
-			msg = {:status => 200, :message => "OK"}
-			render :json => msg
-			return
+			render :json => ok_msg
+		else
+			render :json => error_msg
 		end
-
-		error_msg = {:status => 400, :message => 'There was an error, please try again'}
-		render error_msg
 	end
 end
