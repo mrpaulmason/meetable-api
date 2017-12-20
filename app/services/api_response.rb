f91df24ed90843308cb2a9b3e451364dde11aaf5
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-20 18:04:45 -0500

    fixing error

--- app/services/api_response.rb
+++ app/services/api_response.rb
@@ -1,7 +1,7 @@
 class APIResponse
     class << self
         def response(type:)
-            hash = YAML.load_file("#{Rails.root}/app/services/api_messages.yml")
+            hash = YAML.load_file("#{Rails.root}/app/services/api_response.yml")
             {status: hash[type]["status"], message: hash[type]["message"]}
         end
     end
