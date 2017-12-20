45db964b39a64fd38c06055f83c1895be7f8b4a4
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-20 18:15:19 -0500

    fixing error

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -1,14 +1,7 @@
 class MeetingController < ApplicationController
 	def accept 
-        unless meeting = Meeting.find_by_share_code(params[:id])
-            render :json => APIResponse.response(type: "invalid_referral_code") 
-            return 
-        end
-        
-        unless params.has_key?("phone_number") 
-            render :json => APIResponse.response(type: "invalid_phone_number")
-            return
-        end
+        unless meeting = Meeting.find_by_share_code(params[:id]) render :json => APIResponse.response(type: "invalid_referral_code") and return 
+        unless params.has_key? "phone_number" render :json => APIResponse.response(type: "invalid_phone_number") and return
         
         user = User.find_or_create_by(phone_number: params["phone_number"])
         meeting.invitee_id = user.id
@@ -18,13 +11,12 @@ class MeetingController < ApplicationController
                 client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
                 client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, body: "Welcome to Meetable!")
                 client.messages.create(from: ENV['TWILIO_NUMBER'], to: user.phone_number, media_url: "https://meetable-api.herokuapp.com/vcard")
-            rescue
-                render :json => APIResponse.response(type: "error")
+            rescue => e
+                render :json => APIResponse.response(type: "twilio_error") and return 
                 return
             end
 
-            render :json => APIResponse.response(type: "ok")
-            return
+            render :json => APIResponse.response(type: "ok") and return
         end
 
         render :json => APIResponse.response(type: "error")
