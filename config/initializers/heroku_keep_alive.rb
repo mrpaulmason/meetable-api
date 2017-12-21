0db64dfe02ba0c9679266744f397f4c4e3180f54
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-21 12:02:49 -0500

    adding in a ping for the web to keep it alive

--- config/initializers/heroku_keep_alive.rb
+++ config/initializers/heroku_keep_alive.rb
@@ -5,6 +5,7 @@ if Rails.env.production?
   scheduler.every '10m' do
      require "net/http"
      require "uri"
-     Net::HTTP.get_response(URI.parse(ENV["PING"]))
+     Net::HTTP.get_response(URI.parse(ENV["WEB_PING"]))
+     Net::HTTP.get_response(URI.parse(ENV["API_PING"]))
   end
 end
