ebad88eebceadc025ac508c72d156380c771389f
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2017-12-21 13:02:47 -0500

    first pass at a bet user signup route

--- db/migrate/20171221175320_survey_to_hstore.rb
+++ db/migrate/20171221175320_survey_to_hstore.rb
@@ -1,6 +1,5 @@
 class SurveyToHstore < ActiveRecord::Migration[5.0]
-	enable_extension 'hstore' unless extension_enabled?('hstore')
   def change
-  	change_column :waitlists, :survey, :hstore
+  	change_column :waitlists, :survey, :json
   end
 end
