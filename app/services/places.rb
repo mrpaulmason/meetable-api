2ee214f1fcf617c412d61f59d08e278277707053
Daniel Tyner-Bryan <dan.tyner.bryan@gmail.com>
2018-01-04 16:55:38 -0500

    adding in the lat and long

--- app/services/places.rb
+++ app/services/places.rb
@@ -4,7 +4,7 @@ class Places
             hash = YAML.load_file("#{Rails.root}/app/services/places.yml")
             output = {locations: []}
             hash.each do |k,v|
-            	location = {name: k, address: v["address"]}
+            	location = {name: k, address: v["address"], latitude: v["latitude"], longitude: v["longitude"]}
             	output[:locations].push(location)
             end
             output
