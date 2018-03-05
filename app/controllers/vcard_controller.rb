523aae88b8694b18f29991f0bbce8b9c2d90d945
Daniel Tyner-Bryan <dtyner-bryan@mediamath.com>
2018-03-05 17:03:13 -0500

    Update vcard_controller.rb

--- app/controllers/vcard_controller.rb
+++ app/controllers/vcard_controller.rb
@@ -2,8 +2,8 @@ class VcardController < ApplicationController
 	def card
 		vcard = VCardigan.create
 		vcard.name 'Meetable'
-		vcard.tel '6467599030'
 		vcard.fullname 'Meetable.ai'
+		vcard.tel '6467599030'
 		vcard[:item1].url 'https://meetable.ai'
 		send_data vcard.to_s, :filename => "meetable.vcf"
 	end
