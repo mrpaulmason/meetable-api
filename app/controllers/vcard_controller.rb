015e43ef6812c795bbc6f30e8d30b1d86fcefdc7
Daniel Tyner-Bryan <dtyner-bryan@mediamath.com>
2018-03-05 17:00:25 -0500

    Update vcard_controller.rb

--- app/controllers/vcard_controller.rb
+++ app/controllers/vcard_controller.rb
@@ -4,7 +4,6 @@ class VcardController < ApplicationController
 		vcard.name 'Meetable'
 		vcard.tel '6467599030'
 		vcard.fullname 'Meetable.ai'
-		vcard.photo 'https://media.licdn.com/mpr/mpr/shrinknp_100_100/AAEAAQAAAAAAAAbnAAAAJDFmOTJkNjc2LTZhZWYtNDAxMS04Nzc4LWU1YTFmNjZiMzhiNQ.jpg', :type => 'uri'
 		vcard[:item1].url 'https://meetable.ai'
 		send_data vcard.to_s, :filename => "meetable.vcf"
 	end
