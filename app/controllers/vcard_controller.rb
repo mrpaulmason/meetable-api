cdb83f75adeef6d75ca3202559b6ad55d89adab6
dar326 <darryl.reeves@gmail.com>
2018-03-20 18:28:53 -0400

    Adding Brendan's vCard changes to repo

--- app/controllers/vcard_controller.rb
+++ app/controllers/vcard_controller.rb
@@ -1,11 +1,37 @@
+# class VcardController < ApplicationController
+# 	def card
+# 		vcard = VCardigan.create
+# 		vcard.name 'Meetable'
+# 		vcard.fullname 'Meetable.ai'
+# 		vcard.tel '6467599030'
+# 		vcard[:item1].url 'https://meetable.ai'
+# 		send_data vcard.to_s, :filename => "meetable.vcf"
+# 	end
+# end
+#require 'base64'
+
 class VcardController < ApplicationController
+  # after_filter :set_content_type
+#6467599030
 	def card
-		vcard = VCardigan.create
-		vcard.name 'Meetable'
-		vcard.fullname 'Meetable.ai'
-		vcard.tel '6467599030'
-		vcard[:item1].url 'https://meetable.ai'
-		send_data vcard.to_s, :filename => "meetable.vcf"
+		vcard_template = File.open(File.join(Rails.root, "public/vcard_template.txt"), "rb").read
+		vcard = vcard_template.sub! 'REPLACE_PHONE_NUMBER', '6467599030'
+		send_data vcard, :filename => "meetable.vcf"
+		# ##imgencoded = Base64.encode64(File.open(File.join(Rails.root, "public/m2.jpg"), "rb").read)
+		# vcard = VCardigan.create(:version => '3.0')
+		# vcard.name 'Meetable', ''
+		# vcard.fullname 'Meetable.ai'
+		# vcard.tel '6467599030'
+		# vcard[:item1].url 'https://meetable.ai'
+		# ##vcard.photo 'https://meetable-api-prime.herokuapp.com/m2.jpg', :type => 'JPEG', :value => 'URI'
+		# ##vcard.photo imgencoded, :ENCODING => 'BASE64;JPEG'
+		# send_data vcard.to_s, :filename => "meetable.vcf"
 	end
+
+# 	def set_content_type
+# 		self.content_type = "text/x-vcard; charset=utf-8"
+# #    	self.content_type = "application/foo; bar=1; charset=utf-8"
+#   	end
 end
 
+ 
