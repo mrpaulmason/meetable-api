e143d480b5521b51fd5586afdf3fbc8033aa28a8
dar326 <darryl.reeves@gmail.com>
2018-04-03 17:58:04 -0400

    Using class method for choose_relay

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -13,7 +13,7 @@ class Meeting < ApplicationRecord
 		self.location_type = self.location_type.strip unless self.location_type.nil?
 	end
 
-	def choose_relay initiator:, acceptor:
+	def self.choose_relay initiator:, acceptor:
 		Relay.where("active = ?", true) do |relay|
 			return relay.number unless Meeting.where(:user_id => [initiator.id, acceptor.id])
 								.or(Meeting.where(:invitee_id => [initiator.id, acceptor.id])
