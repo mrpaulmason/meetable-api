6d90a961726c8a5baadb2fd76fb69b9a35b59903
dar326 <darryl.reeves@gmail.com>
2018-04-07 18:36:19 -0400

    generating new share code for second number entered for same meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -7,7 +7,7 @@ class Meeting < ApplicationRecord
 	def generate_share_code
 		# loop until unique share code generated
 		self.share_code = SecureRandom.hex(3)
-		while Meeting.where("share_code = ?", self.share_code)
+		while Meeting.where("share_code = ?", self.share_code).count != 0
 			self.share_code = SecureRandom.hex(3)
 		self.confirmation_code = 6.times.map { rand(1..9) }.join.to_i
 	end
